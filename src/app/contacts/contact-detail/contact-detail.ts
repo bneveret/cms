3f6c297cc124740e2efb9ef4e6e795e906e11523
bneveret <eve20003@byui.edu>
2025-09-27 21:59:41 -0400

    first commit

---
+++ src/app/contacts/contact-detail/contact-detail.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'cms-contact-detail',
+  standalone: false,
+  templateUrl: './contact-detail.html',
+  styleUrl: './contact-detail.css'
+})
+export class ContactDetail {
+  contact: Contact = new Contact(
+    '1',
+    'R. Kent Jackson',
+    'jacksonk@byui.edu',
+    '208-496-3771',
+    'assets/images/jacksonk.jpg',
+    null
+  );
+}
